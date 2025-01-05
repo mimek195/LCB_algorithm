eab77f012b8d7c33e8dd77d9f1e19c7e501f17d1
mimek <94951101+mimek195@users.noreply.github.com>
2025-01-05 15:23:18 +0100

    Update lcb.py
    english

--- lcb.py
+++ lcb.py
@@ -1,18 +1,18 @@
 import numpy
 
-lista_1 = ['a', 'b', 'a', 'm', 'b', 'b', 'a']
-lista_2 = ['a', 'd', 'c', 'b', 'b', 'a', 'n']
+list_1 = ['a', 'b', 'a', 'm', 'b', 'b', 'a']
+list_2 = ['a', 'd', 'c', 'b', 'b', 'a', 'n']
 
-lcb = numpy.zeros((len(lista_1), len(lista_2)), dtype=int)
-wynik = 0
+lcb = numpy.zeros((len(list_1), len(list_2)), dtype=int)
+result = 0
 
-for i in range(len(lista_1)):
-    for j in range(len(lista_1)):
-        if lista_1[i] == lista_2[j]:
+for i in range(len(list_1)):
+    for j in range(len(list_1)):
+        if list_1[i] == list_2[j]:
             lcb[i][j] = lcb[i - 1][j - 1] + 1
         else:
             lcb[i][j] = 0
-        if lcb[i][j] > wynik:
-            wynik = lcb[i][j]
+        if lcb[i][j] > result:
+            result = lcb[i][j]
 
-print(wynik)
+print(result)
